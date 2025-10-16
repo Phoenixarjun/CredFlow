44d4886437f081d592c8e865ef5d30fda4fad957
Naresh B A <naresh.b@prodapt.com>
2025-10-16 22:49:20 +0530

    folder structure created

---
+++ credflow_backend/src/main/java/com/project/credflow/CredflowApplication.java
@@ -0,0 +1,13 @@
+package com.project.credflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredflowApplication.class, args);
+	}
+
+}
